6a5fb5bb274a23c5a77de642553301fff613cbed
Sarvar <sarvarrejametov41@gmail.com>
2026-04-20 15:51:45 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print('hello world')
+
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sarvarrejametov41-cpu/p39-first-project.git
+git push -u origin main
+"""
